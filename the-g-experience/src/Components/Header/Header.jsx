8e361a8c13d219c5970296c6198a57fe9ff47575
dalenromelien <dalenm.romelien@gmail.com>
2024-03-27 23:59:58 -0400

    initialized react app with redux

---
+++ the-g-experience/src/Components/Header/Header.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { Navbar, Nav, Container, Badge, NavDropdown } from "react-bootstrap";
+import { FaShoppingCart, FaUser } from "react-icons/fa";
+import { LinkContainer } from "react-router-bootstrap";
+import logo from "../../assets/logo.png";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg="dark" variant="dark" expand="md" collapseOnSelect>
+        <Container>
+          <LinkContainer to="/">
+            <Navbar.Brand>
+              <img src={logo} className="header-logo" alt="G Experience" />
+            </Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <LinkContainer to="/Events">
+                <Nav.Link>Events</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/Gallery">
+                <Nav.Link>Gallery</Nav.Link>
+              </LinkContainer>
+              <LinkContainer to="/About">
+                <Nav.Link>About</Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
--- the-g-experience/src/index.js
+++ the-g-experience/src/index.js
@@ -1,18 +1,41 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
+import Home from "./Pages/Home";
+import Events from "./Pages/Events";
+import About from "./Pages/About";
+import Gallery from "./Pages/Gallery";
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<Home />} />
+      <Route path="/Events" element={<Events />} />
+      <Route path="/Gallery" element={<Gallery />} />
+      <Route path="/About" element={<About />} />
+    </Route>
+  )
+);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
