1cb91c8a228f8675e81629cdcbbe8d75c0811cc3
TETE IKPEME <tete.ikpeme@gmail.com>
2024-04-17 12:00:28 -0400

    Implemented Gallery and Modal view

--- src/Pages/Gallery.jsx
+++ src/Pages/Gallery.jsx
@@ -1,7 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import Section from "./../Components/Section";
+import galleryImages from "../assets/photos/galleryPhotos";
+import { ModalView } from "../Components/modalView";
 
 const Gallery = () => {
-  return <div>Gallery</div>;
+  const [modalState, setModalState] = useState({
+    isOpen: false,
+    image: "",
+  });
+
+  return (
+    <Section className={"max-w-[80rem]"}>
+      <ModalView
+        {...modalState}
+        closeModal={() => setModalState({ isOpen: false })}
+      />
+
+      <div className="grid gap-2   md:grid-cols-3 md:gap-2 ">
+        {galleryImages.map((image, index) => (
+          <div
+            key={index}
+            className="relative  overflow-clip rounded-sm"
+            onClick={() => setModalState({ isOpen: true, image })}
+          >
+            <img
+              className="w-full object-cover aspect-square "
+              src={image}
+              alt=""
+            />
+          </div>
+        ))}
+      </div>
+    </Section>
+  );
 };
 
 export default Gallery;
---
+++ src/assets/photos/galleryPhotos/index.js
@@ -0,0 +1,23 @@
+import image1 from "./gallery-photo-1.png";
+import image2 from "./gallery-photo-2.jpeg";
+import image3 from "./gallery-photo-3.png";
+import image4 from "./gallery-photo-4.jpeg";
+import image5 from "./gallery-photo-5.png";
+import image6 from "./gallery-photo-6.png";
+import image7 from "./gallery-photo-7.jpeg";
+import image8 from "./gallery-photo-8.jpeg";
+import image9 from "./gallery-photo-9.jpeg";
+
+let galleryImages = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+  image9,
+];
+
+export default galleryImages;
