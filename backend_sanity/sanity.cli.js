762e0576887dd84fce955f07c0c0e523c1dea07c
TETE IKPEME <tete.ikpeme@gmail.com>
2024-06-15 11:51:46 -0400

    Added Sanity

---
+++ backend_sanity/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'v7wm4iz3',
+    dataset: 'production'
+  }
+})
---
+++ backend_sanity/schemaTypes/events.js
@@ -0,0 +1,42 @@
+import {type} from '@testing-library/user-event/dist/cjs/utility/type.js'
+
+export default {
+  name: 'events',
+  type: 'document',
+  title: 'events',
+  fields: [
+    {
+      name: 'title',
+      type: 'string',
+      title: 'Title,',
+    },
+    {
+      name: 'imgUrl',
+      type: 'image',
+      options: {
+        hotspot: true,
+      },
+      title: 'Image URL',
+    },
+    {
+      name: 'description',
+      type: 'string',
+      title: 'Description',
+    },
+    {
+      name: 'venue',
+      type: 'string',
+      title: 'Venue',
+    },
+    {
+      name: 'date',
+      type: 'datetime',
+      title: 'Date',
+    },
+    {
+      name: 'signupLink',
+      type: 'string',
+      title: 'Signup Link',
+    },
+  ],
+}
