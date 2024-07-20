42676dd03d7b62b90db1b916bfcf0478a9aef12b
dalenromelien <dalenm.romelien@gmail.com>
2024-07-20 13:01:15 -0400

    added a little scaffolding for the events component

---
+++ src/Components/Event/Event.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Event = (backgroundColor, events) => {
+  return (
+    <>
+      <div className="event-conatiner">{events.map((event) => {})}</div>
+    </>
+  );
+};
+
+export default Event;
